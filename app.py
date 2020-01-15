129952b33b52a16b50ebd2ac8407e3dec308c9c3
YA <YA-androidapp@users.noreply.github.com>
2020-01-15 12:23:17 +0900

    kura_all_urls

--- app.py
+++ app.py
@@ -44,14 +44,30 @@ def get_kura_index(url):
     print('get_kura_index({})'.format(url))
     # TODO
     # <table class="sakagura-list"><tbody><tr><td><span class="main"><a href="https://">
-    pass
+
+    if url and url.startswith('http'):
+        req = urllib.request.Request(url)
+        response = urllib.request.urlopen(req)
+        html = response.read()
+        # print(html)
+        soup = BeautifulSoup(html, 'lxml')
+        # print(soup.prettify())
+        table = soup.find_all(class_='sakagura-list')
+        if 1 == len(table):
+            kura_links = table[0].select('td > span > a')
+            kura_pref_urls = [i.get('href') for i in kura_links]
+            return kura_pref_urls
+    return []
 
 
 if __name__ == '__main__':
     base_url = get_baseurl()
-    print(base_url)
+    # print(base_url)
     pref_urls = get_pref_index(base_url)
     kura_all_urls = []
     for pref_url in pref_urls:
         kura_pref_urls = get_kura_index(pref_url)
+        # print(kura_pref_urls)
         kura_all_urls.append(kura_pref_urls)
+    kura_all_urls = [item for m in kura_all_urls for item in m]
+    print(kura_all_urls)
